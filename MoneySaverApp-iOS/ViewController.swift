c69fcbab045bc0da2846535c4666fe72a8bbfbca
Michał Moskała <moskala.michal@gmail.com>
2017-03-19 19:46:03 +0100

    Initial commit

---
+++ MoneySaverApp-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michał Moskała on 19.03.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
