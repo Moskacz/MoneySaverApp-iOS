619064d5b0dc1a0ef0ca20d56b57cebf05cb1472
Michał Moskała <moskala.michal@gmail.com>
2017-03-26 20:24:12 +0200

    Allow arbitrary loads for now

--- MoneySaverApp-iOS/ViewController.swift
+++ MoneySaverApp-iOS/ViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 import RESTClient
+import RxSwift
+import MoneySaverFoundationiOS
 
 class ViewController: UIViewController {
 
+    let restClient = RESTClientInterface(baseURL: URL(string: "http://localhost:3000")!)
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let transactionsClient = restClient.transactionsRESTClient()
+        transactionsClient.getTransactions().subscribe(onNext: { (transactions: [Transaction]) in
+            print(transactions)
+        }, onError: { (error: Error) in
+            print(error)
+        }, onCompleted: { 
+            print("completed")
+        }).addDisposableTo(disposeBag)
     }
-
-
+    
 }
 
