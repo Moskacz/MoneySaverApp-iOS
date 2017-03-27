b8b1543eb221d07245d22b1e50286e879d4c69ff
Michał Moskała <moskala.michal@gmail.com>
2017-03-27 19:19:11 +0200

    View controller for adding transaction

---
+++ MoneySaverApp-iOS/Transactions/AddTransactionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddTransactionViewController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michał Moskała on 27.03.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class AddTransactionViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var valueTextField: UITextField!
+}
