36f9ec6162f7ddc1025cefa5c50f6cda2433a1bf
Michał Moskała <moskala.michal@gmail.com>
2017-04-05 19:48:08 +0200

    FlowController protocol

---
+++ MoneySaverApp-iOS/FlowControllers/FlowController.swift
@@ -0,0 +1,13 @@
+//
+//  FlowController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michał Moskała on 05.04.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+protocol FlowController {
+    func startFlow()
+}
