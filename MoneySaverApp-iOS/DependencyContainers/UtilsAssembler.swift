9712a0ffaeaa6e6517c7c7cfd5976e6935379266
Michał Moskała <moskala.michal@gmail.com>
2017-05-14 18:44:15 +0200

    Logger protocol and one implementation - SwiftBeaverLogger

---
+++ MoneySaverApp-iOS/DependencyContainers/UtilsAssembler.swift
@@ -0,0 +1,17 @@
+//
+//  UtilsAssembler.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michał Moskała on 14.05.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+import Dip
+
+class UtilsAssebler: ContainerAssembly {
+    
+    func assembly(container: DependencyContainer) {
+        
+    }
+}
