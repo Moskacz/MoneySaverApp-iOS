6dac7db321f7663f61fb7b4f33dc295f5df1163a
Michał Moskała <moskala.michal@gmail.com>
2017-09-26 19:03:10 +0200

    New tabs - BudgetViewController and StatsViewController

---
+++ MoneySaverApp-iOS/BudgetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetViewController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michal Moskala on 26.09.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class BudgetViewController: UIViewController {
+    
+    override var tabBarItem: UITabBarItem! {
+        get {
+            return UITabBarItem(title: "Budget", image: nil, selectedImage: nil)
+        }
+    }
+}
---
+++ MoneySaverApp-iOS/StatsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StatsViewController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michal Moskala on 26.09.2017.
+//  Copyright © 2017 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+    
+    override var tabBarItem: UITabBarItem! {
+        get {
+            return UITabBarItem(title: "Stats", image: nil, selectedImage: nil)
+        }
+    }
+}
