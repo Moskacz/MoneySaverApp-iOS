20b5b18842d72b98026141f6d0bc15500c734119
Michał Moskała <moskala.michal@gmail.com>
2017-11-03 13:43:36 +0100

    TransactionsListViewController refactor

--- MoneySaverApp-iOS/Transactions/TransactionsListViewController.swift
+++ MoneySaverApp-iOS/Transactions/TransactionsListViewController.swift
@@ -8,12 +8,10 @@
 
 import UIKit
 
-class TransactionsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TransactionsListViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
-    
-    var viewModel: TransactionsListViewModel!
+    @IBOutlet private weak var tableView: UITableView?
+    var viewModel: TransactionsListViewModel?
     
     private let transactionCellIdentifier = "kTransactionCellIdentifier"
     var addTransactionTapCallback: (() -> Void)?
@@ -21,45 +19,47 @@ class TransactionsListViewController: UIViewController, UITableViewDataSource, U
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-        viewModel.attach(updater: TableViewCollectionUpdater(tableView: tableView))
-        viewModel.delegate = self
+        viewModel?.attach(updater: TableViewCollectionUpdater(tableView: tableView))
+        viewModel?.delegate = self
     }
     
     private func setupTableView() {
         let cellNib = UINib(nibName: "TransactionCell", bundle: nil)
-        tableView.register(cellNib, forCellReuseIdentifier: transactionCellIdentifier)
-        tableView.tableFooterView = UIView()
-    }
-    
-    // MARK: UITableViewDataSource
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.transactionsCount()
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: TransactionCell = tableView.dequeueTypedCell(withIdentifier: transactionCellIdentifier)
-        let cellViewModel = viewModel.transactionCellViewModel(atIndex: indexPath.row)
-        cell.update(withViewModel: cellViewModel)
-        return cell
-    }
-    
-    // MARK: UITableViewDelegate
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return TransactionCell.defaultHeight
-    }
-    
-    // MARK: UICollectionViewDataSource
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.transactionsCount()
+        tableView?.register(cellNib, forCellReuseIdentifier: transactionCellIdentifier)
+        tableView?.tableFooterView = UIView()
     }
     
 }
 
+// MARK: UITableViewDataSource
+extension TransactionsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.transactionsCount() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let model = viewModel else { fatalError() }
+        let cell: TransactionCell = tableView.dequeueTypedCell(withIdentifier: transactionCellIdentifier)
+        let cellViewModel = model.transactionCellViewModel(atIndex: indexPath.row)
+        cell.update(withViewModel: cellViewModel)
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+extension TransactionsListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return TransactionCell.defaultHeight
+    }
+}
+
+// MARK: UICollectionViewDelegate
+
+// MARK: TransactionsListViewModelDelegatee
 extension TransactionsListViewController: TransactionsListViewModelDelegate {
     func sumOfTransactionsUpdated(value: Decimal) {
-        titleLabel.text = "\(value)"
+        
     }
 }
