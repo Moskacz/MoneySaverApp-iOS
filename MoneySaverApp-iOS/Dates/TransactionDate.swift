fc46badde39b8b5a2b3a0ce1c4b64c70c6021605
Michał Moskała <moskala.michal@gmail.com>
2018-01-09 20:43:21 +0100

    Added TransactionDate struct which includes all components.

---
+++ MoneySaverApp-iOS/Dates/TransactionDate.swift
@@ -0,0 +1,21 @@
+//
+//  TransactionDate.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michal Moskala on 09.01.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+
+struct TransactionDate {
+    let dayOfWeek: Int
+    let dayOfMonth: Int
+    let dayOfYear: Int
+    let dayOfEra: Int
+    let weekOfYear: Int
+    let month: Int
+    let year: Int
+    let era: Int
+}
+
