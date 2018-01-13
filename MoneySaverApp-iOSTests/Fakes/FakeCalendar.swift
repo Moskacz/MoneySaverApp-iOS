664228d89957b684a65aac1916b6738bee9b8d27
Michał Moskała <moskala.michal@gmail.com>
2018-01-13 10:08:50 +0100

    Added FakeCalendar, implementation of CalendarProtocol used in unit tests

---
+++ MoneySaverApp-iOSTests/Fakes/FakeCalendar.swift
@@ -0,0 +1,24 @@
+//
+//  FakeCalendar.swift
+//  MoneySaverApp-iOSTests
+//
+//  Created by Michal Moskala on 13.01.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+@testable import MoneySaverApp_iOS
+
+class FakeCalendar: CalendarProtocol {
+    func structuredDate(forDate date: Date) -> StructuredDate {
+        fatalError()
+    }
+    
+    func monthName(forDate date: Date) -> String {
+        fatalError()
+    }
+    
+    func yearName(forDate date: Date) -> String {
+        fatalError()
+    }
+}
