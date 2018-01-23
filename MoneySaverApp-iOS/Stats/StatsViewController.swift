b5330278e1d508c8a70985a10bd59a5dfb538892
Michał Moskała <moskala.michal@gmail.com>
2018-01-23 21:27:43 +0100

    Added test graph using ScrollableGraphView

--- MoneySaverApp-iOS/Stats/StatsViewController.swift
+++ MoneySaverApp-iOS/Stats/StatsViewController.swift
@@ -14,4 +14,37 @@ class StatsViewController: UIViewController {
     override lazy var tabBarItem: UITabBarItem! = {
         return UITabBarItem(title: "Stats", image: #imageLiteral(resourceName: "stats"), selectedImage: #imageLiteral(resourceName: "stats"))
     }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTestGraph()
+    }
+    
+    private func setupTestGraph() {
+        let graphView = ScrollableGraphView(frame: .zero, dataSource: self)
+        graphView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(graphView)
+        graphView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        graphView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        graphView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        graphView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        let plot = BarPlot(identifier: "test")
+        graphView.addPlot(plot: plot)
+    }
+}
+
+extension StatsViewController: ScrollableGraphViewDataSource {
+    
+    func label(atIndex pointIndex: Int) -> String {
+        return "\(pointIndex)"
+    }
+    
+    func numberOfPoints() -> Int {
+        return 100
+    }
+    
+    func value(forPlot plot: Plot, atIndex pointIndex: Int) -> Double {
+        return Double(pointIndex)
+    }
 }
