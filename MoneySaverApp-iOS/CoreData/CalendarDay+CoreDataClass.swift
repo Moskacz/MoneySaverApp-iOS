8eb1291ca17ea8df955eaa0e6df4d82188f8075b
Michał Moskała <moskala.michal@gmail.com>
2018-01-28 13:49:50 +0100

    Created new CD entity - CalendarDay

---
+++ MoneySaverApp-iOS/CoreData/CalendarDay+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarDay+CoreDataClass.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michal Moskala on 28.01.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+public class CalendarDay: NSManagedObject {
+
+}
