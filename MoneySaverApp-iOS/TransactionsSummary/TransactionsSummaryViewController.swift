f680915772dbcbfe1bbc9fb6dd807c9f567db3b6
Michał Moskała <moskala.michal@gmail.com>
2018-03-22 20:19:37 +0100

    Added header container controller to transactions list

---
+++ MoneySaverApp-iOS/TransactionsSummary/TransactionsSummaryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionsSummaryViewController.swift
+//  MoneySaverApp-iOS
+//
+//  Created by Michal Moskala on 22.03.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsSummaryViewController: UIViewController {
+    
+}
