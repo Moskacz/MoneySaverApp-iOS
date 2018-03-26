1a7bb0a8c5930a11e4a1303b83ceb52ce90e16ce
Michał Moskała <moskala.michal@gmail.com>
2018-03-26 18:25:29 +0200

    Using GradientView in TransactionsSummaryViewController

--- MoneySaverApp-iOS/TransactionsSummary/TransactionsSummaryViewController.swift
+++ MoneySaverApp-iOS/TransactionsSummary/TransactionsSummaryViewController.swift
@@ -7,7 +7,16 @@
 //
 
 import UIKit
+import MMFoundation
 
 class TransactionsSummaryViewController: UIViewController {
     
+    private var gradientView: GradientView {
+        return view as! GradientView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gradientView.gradient = Gradient(colors: [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)], direction: .skewLeft)
+    }
 }
