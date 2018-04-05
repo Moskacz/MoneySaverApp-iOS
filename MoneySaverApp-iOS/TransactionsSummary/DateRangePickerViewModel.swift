d78c0ebca2ea6b03d9a892dcdd7584ed0cc14352
Michał Moskała <moskala.michal@gmail.com>
2018-04-05 20:36:59 +0200

    Computing titled ranges

--- MoneySaverApp-iOS/TransactionsSummary/DateRangePickerViewModel.swift
+++ MoneySaverApp-iOS/TransactionsSummary/DateRangePickerViewModel.swift
@@ -1,5 +1,23 @@
 import Foundation
 
+struct TitledDateRange {
+    let title: String
+    let range: DateRange
+}
+
 class DateRangePickerViewModel {
     
+    private let calendar: CalendarProtocol
+    
+    init(calendar: CalendarProtocol) {
+        self.calendar = calendar
+    }
+    
+    var ranges: [TitledDateRange] {
+        let today = TitledDateRange(title: "Today", range: .today)
+        let thisMonth = TitledDateRange(title: "Month - \(calendar.monthName(forDate: calendar.now))", range: .thisMonth)
+        let thisYear = TitledDateRange(title: "Year - \(calendar.yearName(forDate: calendar.now))", range: .thisYear)
+        let allTime = TitledDateRange(title: "All", range: .allTime)
+        return [today, thisMonth, thisYear, allTime]
+    }
 }
--- MoneySaverApp-iOSTests/DateRangePickerViewModelTests.swift
+++ MoneySaverApp-iOSTests/DateRangePickerViewModelTests.swift
@@ -7,29 +7,53 @@
 //
 
 import XCTest
+@testable import MoneySaverApp_iOS
 
 class DateRangePickerViewModelTests: XCTestCase {
     
+    var calendarFake: FakeCalendar!
+    var sut: DateRangePickerViewModel!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        calendarFake = FakeCalendar()
+        sut = DateRangePickerViewModel(calendar: calendarFake)
+        fakeDates()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        calendarFake = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    private func fakeDates() {
+        calendarFake.nowToReturn = Date()
+        calendarFake.monthNameToReturn = "April"
+        calendarFake.yearNameToReturn = "2018"
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_rangesContainToday() {
+        let today = sut.ranges.first { $0.range == .today }
+        XCTAssertNotNil(today)
+        XCTAssertEqual(today?.title, "Today")
     }
     
+    func test_rangesContainAllTime() {
+        let allTime = sut.ranges.first { $0.range == .allTime }
+        XCTAssertNotNil(allTime)
+        XCTAssertEqual(allTime?.title, "All")
+    }
+    
+    func test_rangesContainThisMonth_withCurrentMonthName() {
+        let thisMonth = sut.ranges.first { $0.range == .thisMonth }
+        XCTAssertNotNil(thisMonth)
+        XCTAssertEqual(thisMonth?.title, "Month - April")
+    }
+    
+    func test_rangesContainsThisYear_withCurrentYearName() {
+        let thisYear = sut.ranges.first { $0.range == .thisYear }
+        XCTAssertNotNil(thisYear)
+        XCTAssertEqual(thisYear?.title, "Year - 2018")
+    }
 }
