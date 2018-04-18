120ec39ced4ddbdc99260dfb9065f84952d5073d
Michał Moskała <moskala.michal@gmail.com>
2018-04-18 20:33:10 +0200

    Added InMemoryCoreDataStack for testing purposes. This stack operates only in memory and won't touch database.

---
+++ MoneySaverApp-iOSTests/Utils/InMemoryCoreDataStack.swift
@@ -0,0 +1,37 @@
+//
+//  InMemoryCoreDataStack.swift
+//  MoneySaverApp-iOSTests
+//
+//  Created by Michal Moskala on 18.04.2018.
+//  Copyright © 2018 Michal Moskala. All rights reserved.
+//
+
+import Foundation
+import CoreData
+@testable import MoneySaverApp_iOS
+
+class InMemoryCoreDataStack: CoreDataStack {
+    
+    private let container: NSPersistentContainer
+    
+    init() {
+        let container = NSPersistentContainer(name: "DataModel")
+        let storeDescription = NSPersistentStoreDescription()
+        storeDescription.type = NSInMemoryStoreType
+        container.persistentStoreDescriptions = [storeDescription]
+        container.loadPersistentStores { (_, error) in
+            if let loadingError = error {
+                print(loadingError.localizedDescription)
+            }
+        }
+        
+        self.container = container
+    }
+    
+    
+    func loadStores() {}
+    
+    func getViewContext() -> NSManagedObjectContext {
+        return container.viewContext
+    }
+}
