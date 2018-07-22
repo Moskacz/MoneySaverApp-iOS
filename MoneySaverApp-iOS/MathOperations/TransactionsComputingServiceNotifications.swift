9ce492656e328ce1974067a29619593ef54cb074
Michał Moskała <moskala.michal@gmail.com>
2018-07-22 20:43:54 +0200

    Removed duplicated files

--- MoneySaverApp-iOS/MathOperations/TransactionsComputingServiceNotifications.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension Notification.Name {
-    public static let sumChangedNotification = Notification.Name("transactionsSumChangedNotification")
-    public static let monthlyExpensesChangedNotification = Notification.Name("monthlyExpensesChangedNotification")
-}
--- MoneySaverApp-iOS/TransactionCategory/TransactionCategoryCollectionViewCell.swift
+++ MoneySaverApp-iOS/TransactionCategory/TransactionCategoryCollectionViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MoneySaverAppCore
 
 class TransactionCategoryCollectionViewCell: UICollectionViewCell {
     
